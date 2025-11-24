0868ddfca430d4dc0abde29094e8215e7ba8ebeb
KoleszarE <KoleszarE@310-13>
2025-11-24 08:42:00 +0100

    Adatszerkezet és a tesztnevek rögzítése

--- Nevekszurese/Program.cs
+++ Nevekszurese/Program.cs
@@ -4,6 +4,15 @@
     {
         static void Main(string[] args)
         {
+            List<string> list = new List<string>();
+            list.Add("Koleszár Emil");
+            list.Add("Kiss István József");
+            list.Add("rácrz László");
+            list.Add("Farkas1 Aladár");
+            list.Add("Pető@Ilona.hu");
+            list.Add("Pisti");
+            list.Add("12Elemér");
+            list.Add("SzekeresKata");
             while (true)
             {
                 Console.Clear();
